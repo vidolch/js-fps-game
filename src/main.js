259781301ee43b05d15318509824f8b37f9c8e88
Vidol Chalamov <vidol.chalamov@aptsys.net>
2018-02-08 18:08:51 +0200

    Improvements and code cleanup

--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ class Game {
         this.fDepth = 32;			// Maximum rendering distance
         this.mapHeight = 16;
         this.mapWidth = 16;
-        this.resDecrease = 1;
+        this.resDecrease = 4;
+        this.tp1 = Date.now();
+        this.tp2 = Date.now();
+        this.fElapsedTime = 0;
 
         this.screenWidth = this.canvas.width / this.resDecrease;
         this.screenHeight = this.canvas.height / this.resDecrease;
@@ -39,11 +42,17 @@ class Game {
         
         this.createEventListeners();
         this.move();
+        
     }
 
     move() {
-
+		this.tp2 = Date.now();
+		this.fElapsedTime = this.tp2 - this.tp1;
+        this.tp1 = this.tp2;
+        
+        this.context.fillStyle = 'rgb(44, 107, 255)';
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = 'black';
 
         for (let i = 0; i < this.screenWidth; i++) {
             let fRayAngle = (this.player.angle - this.fFOV / 2.0) + (i / this.screenWidth) * this.fFOV;
@@ -59,8 +68,8 @@ class Game {
             while (!bHitWall && distanceToWall < this.fDepth) {
 
                 distanceToWall += stepSize;
-                let nTestX = (this.player.posX + eyeX * distanceToWall);
-                let nTestY = (this.player.posY + eyeY * distanceToWall);
+                let nTestX = Math.floor(this.player.posX + eyeX * distanceToWall);
+                let nTestY = Math.floor(this.player.posY + eyeY * distanceToWall);
 
                 // Test if ray is out of bounds
                 if (nTestX < 0 || nTestX >= this.mapWidth || nTestY < 0 || nTestY >= this.mapHeight) {
@@ -68,8 +77,6 @@ class Game {
                     distanceToWall = this.fDepth;
                 } else {
                     // Ray is inbounds so test to see if the ray cell is a wall block
-                    debugger;
-                    var v = Math.round(nTestX * this.mapWidth + nTestY);
                     if (this.map[Math.round(nTestX * this.mapWidth + nTestY)] === '#') {
                         // Ray has hit wall
                         bHitWall = true;
@@ -81,24 +88,15 @@ class Game {
             let nFloor = this.screenHeight - nCeiling;
 
             // Shader walls based on distance
-            let wShade = 'rgba(119, 119, 119, ' + (1 - distanceToWall * 0.1).toFixed(2) + ')';
-            // if (distanceToWall <= this.fDepth / 4.0)			wShade = 'rgba(119, 119, 119, 1)';	// Very close	
-            // else if (distanceToWall < this.fDepth / 3.0)		wShade = 'rgba(119, 119, 119, .7)';
-            // else if (distanceToWall < this.fDepth / 2.0)		wShade = 'rgba(119, 119, 119, .6)';
-            // else if (distanceToWall < this.fDepth)				wShade = 'rgba(119, 119, 119, .3)';
-            // else    								            wShade = 'rgba(119, 119, 119, 0)';		// Too far away
-
+            let shadeLevel = (distanceToWall * 0.1).toFixed(2) * -1;
+            shadeLevel = shadeLevel < -1 || shadeLevel > 0 ? -1 : shadeLevel;
+            let wShade = this.shadeBlendConvert(shadeLevel, 'rgb(119, 119, 119)');
             this.context.fillStyle = wShade;
             
             for (let y = 0; y < this.screenHeight; y++) {
                 // Each Row
                 let b = 1.0 - ((y - this.screenHeight / 2.0) / (this.screenHeight / 2.0));
                 if (y <= nCeiling) {
-                    // screen[y*this.screenWidth + x] = ' ';
-                    // this.context.fillRect(i,y*this.screenWidth + x,40,40);
-                    this.context.fillStyle = 'rgba(44, 107, 255, ' + (b - 1).toFixed(2) + ')';
-                    this.context.fillRect(i * this.resDecrease, y * this.resDecrease, this.resDecrease, this.resDecrease);//y*this.screenWidth + i
-                    this.context.fillStyle = wShade;
                 } else if (y > nCeiling && y <= nFloor) {
                     // screen[y*this.screenWidth + x] = nShade;
                     this.context.fillRect(i * this.resDecrease, y * this.resDecrease, this.resDecrease, this.resDecrease);//y*this.screenWidth + i
@@ -106,15 +104,8 @@ class Game {
                 else // Floor
                 {
                     // Shade floor based on distance
-                    let gshade;
-                    // if (b < 0.25)		gshade = 'rgba(131, 131, 131, 1)';
-                    // else if (b < 0.5)	gshade = 'rgba(131, 131, 131, .8)';
-                    // else if (b < 0.75)	gshade = 'rgba(131, 131, 131, .6)';
-                    // else if (b < 0.9)	gshade = 'rgba(131, 131, 131, .3)';
-                    // else				gshade = 'rgba(131, 131, 131, 0)';
-                    gshade = 'rgba(147, 67, 2, ' + (1 - b).toFixed(2) + ')';
-                    // screen[] = nShade;
-                    // this.context.fillRect(i,y,40,40); //y*this.screenWidth + i
+                    let gshade = this.shadeBlendConvert((b).toFixed(2) * -1, 'rgb(147, 67, 2)');
+
                     this.context.fillStyle = gshade;
                     this.context.fillRect(i * this.resDecrease, y * this.resDecrease, this.resDecrease, this.resDecrease);//y*this.screenWidth + i
                     this.context.fillStyle = wShade;
@@ -147,7 +138,7 @@ class Game {
         	}
         }
         this.context.fillStyle = 'red';
-        this.context.fillRect(this.player.posX * 10 + 10, this.player.posY * 10 + 10, 10, 10);
+        this.context.fillRect((this.player.posX * 10) + 10, (this.player.posY * 10) + 10, 10, 10);
         this.context.fillStyle = 'black';
 
         // Display Frame
@@ -194,5 +185,37 @@ class Game {
                 self.player.angle += 0.1;
             }
         });
+    }
+    shadeBlendConvert(p, from, to) {
+        if (typeof p != "number" || p < -1 || p > 1 || typeof from != "string" || (from[0] != 'r' && from[0] != '#') || (typeof to != "string" && typeof to != "undefined")) {
+            return null;
+        }
+
+        var i = parseInt,
+            r = Math.round,
+            h = from.length > 9,
+            h = typeof to == "string" ? to.length > 9 ? true : to == "c" ? !h : false : h,
+            b = p < 0,
+            p = b ? p * -1 : p,
+            to = to && to != "c" ? to : b ? "#000000" : "#FFFFFF",
+            f = this.sbcRip(from, i),
+            t = this.sbcRip(to, i);
+        if (!f || !t) return null;
+        if (h) return "rgb(" + r((t[0] - f[0]) * p + f[0]) + "," + r((t[1] - f[1]) * p + f[1]) + "," + r((t[2] - f[2]) * p + f[2]) + (f[3] < 0 && t[3] < 0 ? ")" : "," + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 10000) / 10000 : t[3] < 0 ? f[3] : t[3]) + ")");
+        else return "#" + (0x100000000 + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 255) : t[3] > -1 ? r(t[3] * 255) : f[3] > -1 ? r(f[3] * 255) : 255) * 0x1000000 + r((t[0] - f[0]) * p + f[0]) * 0x10000 + r((t[1] - f[1]) * p + f[1]) * 0x100 + r((t[2] - f[2]) * p + f[2])).toString(16).slice(f[3] > -1 || t[3] > -1 ? 1 : 3);
+    }
+    sbcRip(d, i) {
+        var l = d.length,
+            RGB = new Object();
+        if (l > 9) {
+            d = d.split(",");
+            if (d.length < 3 || d.length > 4) return null;
+            RGB[0] = i(d[0].slice(4)), RGB[1] = i(d[1]), RGB[2] = i(d[2]), RGB[3] = d[3] ? parseFloat(d[3]) : -1;
+        } else {
+            if (l == 8 || l == 6 || l < 4) return null;
+            if (l < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (l > 4 ? d[4] + "" + d[4] : "");
+            d = i(d.slice(1), 16), RGB[0] = d >> 16 & 255, RGB[1] = d >> 8 & 255, RGB[2] = d & 255, RGB[3] = l == 9 || l == 5 ? r(((d >> 24 & 255) / 255) * 10000) / 10000 : -1;
+        }
+        return RGB;
     }
 }
