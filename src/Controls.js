2565912e6cb612e4f93c7189ccfa014e49ea9630
Vidol Chalamov <vi4ovi4o96@gmail.com>
2018-02-11 11:56:17 +0200

    Updated sprites Refactoring over controls and visual utilities Added angle for camera view New map!!!

---
+++ src/Controls.js
@@ -0,0 +1,79 @@
+export class Controls {
+    constructor(options) {
+        this.keysBindings = {};
+
+        if(options !== undefined) {
+            if(options.hasOwnProperty('pointerLock')) {
+                if (!!options['pointerLock']) {
+                    if (typeof options['canvas'] === undefined) {
+                        throw "In order to utilize pointer lock, provide convas in options!";
+                    }
+                    this.useMouse = false;
+                    this.bindPointer(options['canvas'], options['pointerCallout']);
+                }
+            }
+        }
+        this.createEventListeners();
+    }
+
+    bindKeys(keys, callout) {
+        if(Array.isArray(keys)) {
+            for (let i = 0; i < keys.length; i++) {
+                this.keysBindings[keys[i]] = callout;
+            }
+        } else {
+            throw "Keys parameter should be an Array!";
+        }
+    }
+
+    bindKey(key, callout) {
+        if(typeof keys === 'String') {
+            this.keysBindings[keys] = callout;
+        } else {
+            throw "Key parameter should be String!";
+        }
+    }
+
+    createEventListeners() {
+        let self = this;
+        let fElapsedTime = 0.1
+        document.addEventListener("keydown", function (e) {
+            e = e || window.event;
+            if(self.keysBindings.hasOwnProperty(e.keyCode)) {
+                self.keysBindings[e.keyCode]();
+            }
+        });
+       
+    }
+    bindPointer(canvas, pointerCallout) {
+        let self = this;
+        canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+
+        document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+
+        canvas.onclick = function () {
+            self.useMouse = true;
+            canvas.requestPointerLock();
+        };
+        document.addEventListener('pointerlockchange', function (e) {
+            self.lockChange(canvas, pointerCallout, objContext);
+        }, false);
+        document.addEventListener('mozpointerlockchange', function (e) {
+            self.lockChange(canvas, pointerCallout, objContext);
+        }, false);
+    }
+    lockChange(canvas, pointerCallout) {
+        let self = this;
+        if (document.pointerLockElement === canvas ||
+            document.mozPointerLockElement === canvas) {
+            console.log('The pointer lock status is now locked');
+            document.addEventListener("mousemove", function (e) {
+                if(!self.useMouse) return;
+                pointerCallout(e);
+            }, false);
+        } else {
+            console.log('The pointer lock status is now unlocked');
+            self.useMouse = false;
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-class Game {
+import { VisualUtils } from './VisualUtils.js';
+import { Controls } from './Controls.js';
+
+export class Game {
     constructor() {
+        this.controls;
+
         this.step = 40;
 
         this.canvas = document.getElementById("myCanvas");
@@ -7,60 +12,84 @@ class Game {
         this.fFOV = 3.14159 / 4.0;	// Field of View
         this.fSpeed = 2;
         this.fDepth = 32;			// Maximum rendering distance
-        this.mapHeight = 16;
-        this.mapWidth = 16;
+        this.mapHeight = 32;
+        this.mapWidth = 32;
         this.resDecrease = 1;
         this.tp1 = Date.now();
         this.tp2 = Date.now();
         this.fElapsedTime = 0;
+        
+        this.mousePositionX = 0;
 
         this.screenWidth = this.canvas.width / this.resDecrease;
         this.screenHeight = this.canvas.height / this.resDecrease;
-        
+
         this.player = {
             posX: 8,
             posY: 8,
             angle: 0
         }
-            
-        this.map =  "#########.......";
-        this.map += "#...............";
-        this.map += "#.......########";
-        this.map += "#..............#";
-        this.map += "#......##......#";
-        this.map += "#......##......#";
-        this.map += "#..............#";
-        this.map += "###............#";
-        this.map += "##.............#";
-        this.map += "#......####..###";
-        this.map += "#......#.......#";
-        this.map += "#......#.......#";
-        this.map += "#..............#";
-        this.map += "#......#########";
-        this.map += "#..............#";
-        this.map += "################";
-        
+
+        this.map =  "#########.......#########.......";
+		this.map += "#...............#...............";
+		this.map += "#.......#########.......########";
+		this.map += "#..............##..............#";
+		this.map += "#......##......##......##......#";
+		this.map += "#......##..............##......#";
+		this.map += "#..............##..............#";
+		this.map += "###............####............#";
+		this.map += "##.............###.............#";
+		this.map += "#............####............###";
+		this.map += "#..............................#";
+		this.map += "#..............##..............#";
+		this.map += "#..............##..............#";
+		this.map += "#...........#####...........####";
+		this.map += "#..............................#";
+		this.map += "###..####....########....#######";
+		this.map += "####.####.......######..........";
+		this.map += "#...............#...............";
+		this.map += "#.......#########.......##..####";
+		this.map += "#..............##..............#";
+		this.map += "#......##......##.......#......#";
+		this.map += "#......##......##......##......#";
+		this.map += "#..............##..............#";
+		this.map += "###............####............#";
+		this.map += "##.............###.............#";
+		this.map += "#............####............###";
+		this.map += "#..............................#";
+		this.map += "#..............................#";
+		this.map += "#..............##..............#";
+		this.map += "#...........##..............####";
+		this.map += "#..............##..............#";
+		this.map += "################################";
+
         this.nCeiling = 0;
         this.nFloor = 0;
         this.wallTexture = document.getElementById('source');
         this.wallPatern = null;
 
-        this.createEventListeners();
+        this.middleCorrdinates = {};
+
+
+        this.tracker = document.getElementById('tracker');
+
+        this.animation;
+
+        this.createControls();
         this.move();
-        
     }
 
     move() {
         this.wallPatern = this.wallPatern === null ? this.context.createPattern(this.wallTexture, 'repeat') : this.wallPatern; // Create a pattern with this image, and set it to "repeat".
-		this.tp2 = Date.now();
-		this.fElapsedTime = this.tp2 - this.tp1;
+        this.tp2 = Date.now();
+        this.fElapsedTime = this.tp2 - this.tp1;
         this.tp1 = this.tp2;
-        
+
         this.context.fillStyle = 'rgb(44, 107, 255)';
         this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
         var linearGradient1 = this.context.createLinearGradient(0, this.canvas.height, 0, this.canvas.height / 2);
         linearGradient1.addColorStop(0, 'rgb(147, 67, 2)');
-        linearGradient1.addColorStop(0.65, this.shadeBlendConvert(-0.8, 'rgb(147, 67, 2)'));
+        linearGradient1.addColorStop(0.65, VisualUtils.shadeBlendConvert(-0.8, 'rgb(147, 67, 2)'));
         linearGradient1.addColorStop(1, 'rgb(0, 0, 0)');
         this.context.fillStyle = linearGradient1;
         this.context.fillRect(0, this.canvas.height / 2, this.canvas.width, this.canvas.height / 2);
@@ -82,6 +111,7 @@ class Game {
             bHitWall = false;		// Set when ray hits wall block
             eyeX = Math.sin(fRayAngle); // Unit vector for ray in player space
             eyeY = Math.cos(fRayAngle);
+            if(i === this.screenWidth / 2) this.middleCorrdinates = {x: eyeX, y: eyeY}; 
 
             let fSampleX = 0.0;
             // Incrementally cast ray from player, along ray angle, testing for 
@@ -103,26 +133,28 @@ class Game {
                         bHitWall = true;
 
                         // Determine where ray has hit wall. Break Block boundary
-						// int 4 line segments
-						let fBlockMidX = nTestX + 0.5;
+                        // int 4 line segments
+                        let fBlockMidX = nTestX + 0.5;
                         let fBlockMidY = nTestY + 0.5;
-                        
-						let fTestPointX = this.player.posX + eyeX * distanceToWall;
-                        let fTestPointY = this.player.posY + eyeY * distanceToWall;
-                        
-						let fTestAngle = Math.atan2((fTestPointY - fBlockMidY), (fTestPointX - fBlockMidX));
 
-						if (fTestAngle >= -3.14159 * 0.25 && fTestAngle < 3.14159 * 0.25)
-							fSampleX = fTestPointY - nTestY;
-						if (fTestAngle >= 3.14159 * 0.25 && fTestAngle < 3.14159 * 0.75)
-							fSampleX = fTestPointX - nTestX;
-						if (fTestAngle < -3.14159 * 0.25 && fTestAngle >= -3.14159 * 0.75)
-							fSampleX = fTestPointX - nTestX;
-						if (fTestAngle >= 3.14159 * 0.75 || fTestAngle < -3.14159 * 0.75)
-							fSampleX = fTestPointY - nTestY;
+                        let fTestPointX = this.player.posX + eyeX * distanceToWall;
+                        let fTestPointY = this.player.posY + eyeY * distanceToWall;
+
+                        let fTestAngle = Math.atan2((fTestPointY - fBlockMidY), (fTestPointX - fBlockMidX));
+
+                        if (fTestAngle >= -3.14159 * 0.25 && fTestAngle < 3.14159 * 0.25)
+                            fSampleX = fTestPointY - nTestY;
+                        if (fTestAngle >= 3.14159 * 0.25 && fTestAngle < 3.14159 * 0.75)
+                            fSampleX = fTestPointX - nTestX;
+                        if (fTestAngle < -3.14159 * 0.25 && fTestAngle >= -3.14159 * 0.75)
+                            fSampleX = fTestPointX - nTestX;
+                        if (fTestAngle >= 3.14159 * 0.75 || fTestAngle < -3.14159 * 0.75)
+                            fSampleX = fTestPointY - nTestY;
                     }
                 }
             }
+            fSampleX *= 100;
+            fSampleX = Math.floor(fSampleX / (100 / 288));
             // Calculate distance to ceiling and floor
             this.nCeiling = (this.screenHeight / 2.0) - (this.screenHeight / distanceToWall);
             this.nFloor = this.screenHeight - this.nCeiling;
@@ -131,40 +163,32 @@ class Game {
             let shadeLevel = (distanceToWall * 0.1).toFixed(2);// * -1;
             // shadeLevel = shadeLevel < -1 || shadeLevel > 0 ? -1 : shadeLevel;
             // let wShade = this.shadeBlendConvert(shadeLevel, 'rgb(119, 119, 119)');    
-            
+
             let heightToDraw = 0;
             let firstY = -1;
             for (let y = 0; y < this.screenHeight; y++) {
                 // Each Row
-                let b = 1.0 - ((y - this.screenHeight / 2.0) / (this.screenHeight / 2.0));
+                // let b = 1.0 - ((y - this.screenHeight / 2.0) / (this.screenHeight / 2.0));
                 if (y <= this.nCeiling) { // roof
                 } else if (y > this.nCeiling && y <= this.nFloor) {
                     // Draw Wall
-					if (distanceToWall < this.fDepth)
-					{
-						let fSampleY = (y - this.nCeiling) / (this.nFloor - this.nCeiling);
+                    if (distanceToWall < this.fDepth) {
+                        // let fSampleY = (y - this.nCeiling) / (this.nFloor - this.nCeiling);
+                        // fSampleY *= 100;
+                        // fSampleY = Math.floor(fSampleY / (100 / 36));
                         // Draw(x, y, spriteWall->SampleGlyph(fSampleX, fSampleY), spriteWall->SampleColour(fSampleX, fSampleY));
                         // debugger;
                         // this.wallPaternrotate(60 * Math.PI / 180);
                         // this.context.fillStyle = this.wallPatern; 
                         // this.context.fillRect(i * this.resDecrease, y * this.resDecrease, this.resDecrease, this.resDecrease);
-                        this.context.drawImage(
-                            this.wallTexture, 
-                            fSampleX, 
-                            fSampleY, 
-                            this.resDecrease, this.resDecrease, 
-                            i * this.resDecrease, 
-                            y * this.resDecrease, 
-                            this.resDecrease, this.resDecrease);
-					}
-                    else 
-                    {
-                        this.context.fillStyle = 'rgb(0, 0, 0)';
-                        this.context.fillRect(i * this.resDecrease, y * this.resDecrease, this.resDecrease, this.resDecrease);  
+                    }
+                    else {
+                        // this.context.fillStyle = 'rgb(0, 0, 0)';
+                        // this.context.fillRect(i * this.resDecrease, y * this.resDecrease, this.resDecrease, this.resDecrease);  
                     }
 
-                    // heightToDraw += this.resDecrease;
-                    // firstY = firstY === -1 ? y : firstY;
+                    heightToDraw += this.resDecrease;
+                    firstY = firstY === -1 ? y : firstY;
                 } else { // Floor
                     // Shade floor based on distance
                     // let gshade = this.shadeBlendConvert((b).toFixed(2) * -1, 'rgb(147, 67, 2)');
@@ -174,92 +198,115 @@ class Game {
             }
             // this.context.fillStyle = this.wallPatern;    
             // this.context.fillRect(i * this.resDecrease, firstY * this.resDecrease, this.resDecrease, heightToDraw);
-            // this.context.fillStyle = 'rgba(0, 0, 0, ' + shadeLevel + ')';    
-            // this.context.fillRect(i * this.resDecrease, firstY * this.resDecrease, this.resDecrease, heightToDraw);
+            this.context.drawImage(
+                this.wallTexture,
+                fSampleX,
+                0,
+                this.resDecrease, 288,
+                i * this.resDecrease,
+                firstY,
+                this.resDecrease, heightToDraw);
+            this.context.fillStyle = 'rgba(0, 0, 0, ' + shadeLevel + ')';
+            this.context.fillRect(i * this.resDecrease, firstY * this.resDecrease, this.resDecrease, heightToDraw);
         }
         this.context.fillStyle = 'black';
-        this.context.fillStyle = 'white';        
+        this.context.fillStyle = 'white';
         for (let nx = 0; nx < this.mapWidth; nx++) {
-        	for (let ny = 0; ny < this.mapHeight; ny++)
-        	{
-                if(this.map[ny * this.mapWidth + nx] === '#') {
-                    this.context.fillRect((nx * 10) + 10, (ny * 10) + 10, 10, 10);
+            for (let ny = 0; ny < this.mapHeight; ny++) {
+                if (this.map[ny * this.mapWidth + nx] === '#') {
+                    this.context.fillRect((nx * 4) + 10, (ny * 4) + 10, 4, 4);
                 }
-        	}
+            }
         }
         this.context.fillStyle = 'red';
-        this.context.fillRect((this.player.posY * 10) + 10, (this.player.posX * 10) + 10, 4, 4);
+        this.context.fillRect((this.player.posY * 4) + 10, (this.player.posX * 4) + 10, 2, 2);
+        this.context.beginPath();
+        this.context.moveTo((this.player.posY * 4) + 10, (this.player.posX * 4) + 10);
+        
+        // nTestX = Math.floor(this.player.posX + eyeX * distanceToWall);
+        // nTestY = Math.floor(this.player.posY + eyeY * distanceToWall);
+        this.context.lineTo(
+            Math.floor(this.player.posY + this.middleCorrdinates.y * 5) * 4 + 10,
+            // ((this.middleCorrdinates.y * 100) * 10) + 10,
+            Math.floor(this.player.posX + this.middleCorrdinates.x * 5) * 4 + 10
+            // ((this.middleCorrdinates.x * 100) * 10) + 10
+        );
+        this.context.strokeStyle = 'red';
+        this.context.stroke();
         this.context.fillStyle = 'black';
 
         let self = this;
         setTimeout(self.move.bind(self), 1);
     }
 
-    createEventListeners() {
+    createControls() {
         let self = this;
-        let fElapsedTime = 0.1
-        document.addEventListener("keydown", function (e) {
-            e = e || window.event;
-            if (e.keyCode == '38') {
-                // up arrow
-                self.player.posX += Math.sin(self.player.angle) * self.fSpeed * fElapsedTime;
-                self.player.posY += Math.cos(self.player.angle) * self.fSpeed * fElapsedTime;
-                if (self.map[Math.floor(self.player.posX) * self.mapWidth + Math.floor(self.player.posY)] === '#')
-                {
-                    self.player.posX -= Math.sin(self.player.angle) * self.fSpeed * fElapsedTime;
-                    self.player.posY -= Math.cos(self.player.angle) * self.fSpeed * fElapsedTime;
-                }	
+        this.controls = new Controls({
+            pointerLock: true,
+            canvas: self.canvas,
+            pointerCallout: function(e) {
+                self.updatePosition(e);
             }
-            else if (e.keyCode == '40') {
-                // down arrow
-                self.player.posX -= Math.sin(self.player.angle) * self.fSpeed * fElapsedTime;
-                self.player.posY -= Math.cos(self.player.angle) * self.fSpeed * fElapsedTime;
-                if (self.map[Math.floor(self.player.posX) * self.mapWidth + Math.floor(self.player.posY)] === '#')
-                {
-                    self.player.posX += Math.sin(self.player.angle) * self.fSpeed * fElapsedTime;
-                    self.player.posY += Math.cos(self.player.angle) * self.fSpeed * fElapsedTime;
-                }	
+        });
+
+        // up arrow OR "W" key
+        this.controls.bindKeys(['38', '87'], function() {
+            self.player.posX += self.calcNextPlayerPositionX();
+            self.player.posY += self.calcNextPlayerPositionY();
+            if (self.checkForWall()) {
+                self.player.posX -= self.calcNextPlayerPositionX();
+                self.player.posY -= self.calcNextPlayerPositionY();
             }
-            else if (e.keyCode == '37') {
-                // left arrow
-                self.player.angle -= 0.1;
+        });
+        // down arrow OR "S" key
+        this.controls.bindKeys(['40', '83'], function() {
+             self.player.posX -= self.calcNextPlayerPositionX();
+             self.player.posY -= self.calcNextPlayerPositionY();
+             if (self.checkForWall()) {
+                 self.player.posX += self.calcNextPlayerPositionX();
+                 self.player.posY += self.calcNextPlayerPositionY();
+             }
+        });
+        // left arrow OR "D" key
+        this.controls.bindKeys(['37', '65'], function() {
+            self.player.posX += self.calcNextPlayerPositionX();
+            self.player.posY -= self.calcNextPlayerPositionY();
+            if (self.checkForWall()) {
+                self.player.posX -= self.calcNextPlayerPositionX();
+                self.player.posY += self.calcNextPlayerPositionY();
             }
-            else if (e.keyCode == '39') {
-                // right arrow
-                self.player.angle += 0.1;
+        });
+        // right arrow OR "A" key
+        this.controls.bindKeys(['39', '68'], function() {
+            self.player.posX -= self.calcNextPlayerPositionX();
+            self.player.posY += self.calcNextPlayerPositionY();
+            if (self.checkForWall()) {
+                self.player.posX += self.calcNextPlayerPositionX();
+                self.player.posY -= self.calcNextPlayerPositionY();
             }
         });
     }
-    shadeBlendConvert(p, from, to) {
-        if (typeof p != "number" || p < -1 || p > 1 || typeof from != "string" || (from[0] != 'r' && from[0] != '#') || (typeof to != "string" && typeof to != "undefined")) {
-            return null;
+    calcNextPlayerPositionX() {
+        return Math.sin(this.player.angle) * this.fSpeed * 0.1;
+    }
+    calcNextPlayerPositionY() {
+        return Math.cos(this.player.angle) * this.fSpeed * 0.1;
+    }
+    checkForWall() {
+        return this.map[Math.floor(this.player.posX) * this.mapWidth + Math.floor(this  .player.posY)] === '#';
+    }
+    updatePosition(e) {
+        if(e.movementX > 0) {
+            this.player.angle += (e.movementX) * 0.005;
+            console.log("Left " + e.movementX);
         }
+        if(e.movementX < 0) {
+            this.player.angle += ( e.movementX) * 0.005;
+            console.log("Right " + e.movementX);
+        }
+    }
+}
 
-        var i = parseInt,
-            r = Math.round,
-            h = from.length > 9,
-            h = typeof to == "string" ? to.length > 9 ? true : to == "c" ? !h : false : h,
-            b = p < 0,
-            p = b ? p * -1 : p,
-            to = to && to != "c" ? to : b ? "#000000" : "#FFFFFF",
-            f = this.sbcRip(from, i),
-            t = this.sbcRip(to, i);
-        if (!f || !t) return null;
-        if (h) return "rgb(" + r((t[0] - f[0]) * p + f[0]) + "," + r((t[1] - f[1]) * p + f[1]) + "," + r((t[2] - f[2]) * p + f[2]) + (f[3] < 0 && t[3] < 0 ? ")" : "," + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 10000) / 10000 : t[3] < 0 ? f[3] : t[3]) + ")");
-        else return "#" + (0x100000000 + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 255) : t[3] > -1 ? r(t[3] * 255) : f[3] > -1 ? r(f[3] * 255) : 255) * 0x1000000 + r((t[0] - f[0]) * p + f[0]) * 0x10000 + r((t[1] - f[1]) * p + f[1]) * 0x100 + r((t[2] - f[2]) * p + f[2])).toString(16).slice(f[3] > -1 || t[3] > -1 ? 1 : 3);
-    }
-    sbcRip(d, i) {
-        var l = d.length,
-            RGB = new Object();
-        if (l > 9) {
-            d = d.split(",");
-            if (d.length < 3 || d.length > 4) return null;
-            RGB[0] = i(d[0].slice(4)), RGB[1] = i(d[1]), RGB[2] = i(d[2]), RGB[3] = d[3] ? parseFloat(d[3]) : -1;
-        } else {
-            if (l == 8 || l == 6 || l < 4) return null;
-            if (l < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (l > 4 ? d[4] + "" + d[4] : "");
-            d = i(d.slice(1), 16), RGB[0] = d >> 16 & 255, RGB[1] = d >> 8 & 255, RGB[2] = d & 255, RGB[3] = l == 9 || l == 5 ? r(((d >> 24 & 255) / 255) * 10000) / 10000 : -1;
-        }
-        return RGB;
-    }
+window.onload = function() {
+    new Game();
 }
